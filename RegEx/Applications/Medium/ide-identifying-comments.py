af01f3508b1b48fce452b79f12a8e9abc8d84d5f
John Kane <jokane@catmktg.com>
2021-01-12 13:48:18 -0600

    work on identifying comments

--- RegEx/Applications/Medium/ide-identifying-comments.py
+++ RegEx/Applications/Medium/ide-identifying-comments.py
@@ -1,12 +1,18 @@
 # Solves this application problem: https://www.hackerrank.com/challenges/detect-the-domain-name
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import sys, re
+# looking at read vs readlines...read in as one string or as each line on new page
 #input_data =  sys.stdin.readlines()
-input_data = open("C:\\Users\\jokane\\Desktop\\HackerRank\\RegEx\\Applications\\Medium\\ide-identifying-comments-input1.txt", "r").readlines()
-for line in input_data[1:]:
-    regex = re.findall(
-        pattern = "(\/\/.*$)|(\/\*\*.*\n.*\*\*\/)",
-        string = line 
-    )
-    print(regex)
-    del regex
+#input_data = open("C:\\Users\\jokane\\Desktop\\HackerRank\\RegEx\\Applications\\Medium\\ide-identifying-comments-input1.txt", "r").readlines()
+input_data = open("C:\\Users\\jokane\\Desktop\\HackerRank\\RegEx\\Applications\\Medium\\ide-identifying-comments-input1.txt", "r").read() 
+regex = re.findall(
+        pattern = "(\/\/.*(?=\n))|(\/\*.*\n{1,}.*\*\/)",
+        string = input_data,
+        )
+for t in regex:
+    for i in t:
+        if i != '':
+            print(i)
+            
+            
+            
